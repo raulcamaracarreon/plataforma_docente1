6fea4bf80d5e2ffd95ce08e6cfdf1c5a34925000
raulcamaracarreon <125494150+raulcamaracarreon@users.noreply.github.com>
2023-02-26 11:42:18 -0600

    Update pruebota.py

--- pruebota.py
+++ pruebota.py
@@ -2,7 +2,8 @@ import streamlit as st
 from cuestionario import cuestionario_page
 from respuestas import respuestas_page
 from Videoteca import videoteca_page
-
+import locale
+locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')
 # Configuración de página
 st.set_page_config(page_title="Cuestionario de autoevaluación de prácticas docentes", page_icon=":books:")
 
